01595413d7f83dbc98b9be61174ab64111d2b71c
hieuhoang <hieu.hoang@employmenthero.com>
2019-04-12 22:28:56 +0700

    Add antd and react-app-rewired

--- src/App.js
+++ src/App.js
@@ -1,4 +1,5 @@
 import React, { Component } from 'react';
+import Button from 'antd/lib/button';
 import logo from './logo.svg';
 import './App.css';
 
@@ -19,6 +20,7 @@ class App extends Component {
           >
             Learn React
           </a>
+          <Button type="primary">Button</Button>
         </header>
       </div>
     );
